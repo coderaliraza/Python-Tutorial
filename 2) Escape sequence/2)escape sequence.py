69f1b37dc13065558354f4ceab75ca9fb785a316
Ali Raza <coderaliraza860@gmail.com>
2024-11-20 22:01:00 +0500

    Add files via upload

---
+++ 2) Escape sequence/2)escape sequence.py
@@ -0,0 +1,22 @@
+# single quotes
+print('I\'m ali')
+# Double Quotes
+print("Hello \"hello world\"world")
+print("I\"m Ali Raza")
+# backslash
+# print("this is a  \ Backslash")
+print("This is a Backslash\\")
+print("THis is a BackSlash\\\\")
+# New Line
+print("Line A")
+print("Line B")
+print("this is a line A \nthis is a line B")
+# Carriage return
+print("Hello world")
+print("Hel \rlo  ali world")
+# Tab
+print("I'm \t Ali")
+# Backspace
+print("hee\bllo \bali")
+# form feed
+print("hel\flo \f world")
