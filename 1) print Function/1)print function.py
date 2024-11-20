010f2c7c5827ab960bb3c91a4824eec059932526
Ali Raza <coderaliraza860@gmail.com>
2024-11-20 21:58:00 +0500

    Add files via upload

---
+++ 1) print Function/1)print function.py
@@ -0,0 +1,9 @@
+print("hello world")
+print('hello world')
+print('ali "raza" python')
+print("ali 'raza' python")
+print("I'm ali raza")
+
+# print("hello "word" ali") syntax error
+# print('hello 'world' ali raza') syntax error
+ 
