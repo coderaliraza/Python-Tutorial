6d66f505d7730c0c239df858e6e1ae7159004f59
Ali Raza <coderaliraza860@gmail.com>
2024-11-21 11:39:35 +0500

    Add files via upload

---
+++ 9) Python User input/9)Python User input.py
@@ -0,0 +1,10 @@
+#user input
+# function
+user_name = input("Enter Your Name: ")
+print("Your Name is:" + " " + user_name)
+
+user_name = input("Enter Your Name: ")
+age       = input("Enter Your Age: ")
+print("Your Name is: " + user_name + " " + "\nYour Age is: " + age)
+
+
