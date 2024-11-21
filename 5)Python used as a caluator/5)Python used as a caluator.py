b0f00e310fed8f123ad242524ec0d26411fd2e33
Ali Raza <coderaliraza860@gmail.com>
2024-11-21 11:32:09 +0500

    Add files via upload

---
+++ 5)Python used as a caluator/5)Python used as a caluator.py
@@ -0,0 +1,27 @@
+#addition in python
+print(2+2)
+print(3+2)
+#Minus/ subtraction
+print(3-1)
+print(2-3)
+#multiplaction
+print(2*3)
+print(3*3)
+# mathematical calculation in all in one way (Addition, subtraction, Multiplication)
+print(2+3*2)
+print(2-3*3)
+print(3*2-2)
+#Float Division
+print(4/2) #flaotion point answer only point answer give you
+print(2/4) #flaoating point answer only point answer give you
+#interger Division
+print(4//2)
+print(3//9)
+#modulo division
+print(6%2)
+#Exponent (Square root)
+print(2**3)
+print(2**0.5)
+print(round(2**0.5,2))
+#precedence rule
+print((2+3) *3)
