4f4914def8039e5c4792091d34741a360f45b29f
Ali Raza <coderaliraza860@gmail.com>
2024-11-21 11:36:48 +0500

    Add files via upload

---
+++ 7)Python variables/7)Python variables.py
@@ -0,0 +1,33 @@
+#string
+name = "Python"
+print(name)
+name = "Python, Java, HTML, Css"
+print(name)
+#interger
+number1 = 2
+print(number1)
+number1 = 3
+print(number1)
+# write variable rule name
+myname = "Python"
+print(myname)
+my_name = "python"
+print(my_name)
+_my_name = "python"
+print(_my_name)
+myName = "python"
+print(myName)
+MYNAME = "python"
+print(MYNAME)
+myname2 = "python"
+
+#camel case
+myFirstName = "python"
+print(myFirstName)
+#pascal case
+MyFirstName = "python"
+print(MyFirstName)
+# snake case
+my_first_name = "Python"
+print(my_first_name)
+
