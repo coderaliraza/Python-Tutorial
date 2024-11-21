cefeb0c235bf647779e12a3adc178f43364d5567
Ali Raza <coderaliraza860@gmail.com>
2024-11-21 11:35:55 +0500

    Add files via upload

---
+++ 6) Exercise 1/Excersice 1.py
@@ -0,0 +1,7 @@
+# --- print these following lines---
+# This is a \\ Double backslash
+# This is a /\/\/\/\/\ mountain
+# He is a         Awesome(Use escape sequence instead of manual spaces)
+# \" \n \t \' (Print this as an output)
+
+
