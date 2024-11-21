07adb0ba248d302e63a24a95a670568971dd8fd1
Ali Raza <coderaliraza860@gmail.com>
2024-11-21 17:12:01 +0500

    Add files via upload

---
+++ 13)Python string formatting/13)Python string formatting.py
@@ -0,0 +1,6 @@
+name = "ali"
+age = 24
+address = "FSD"
+# print("Hello " + name + " " + "Your Age is " + str(age)) #complete Syntax/ Ugly syntax
+print("Hello {} Your age is {} your address is {}".format(name, age, address))
+# print(f"Hello {name} Your Age is {age} Your address is {address}")
