dd96234ef0ee453a348a4c6cbf6d2d36d0d89163
Ali Raza <coderaliraza860@gmail.com>
2024-11-21 11:46:09 +0500

    Add files via upload

---
+++ 10)Python int() function/10)Python int() function.py
@@ -0,0 +1,9 @@
+# number_one = int(input("Enter First Number: "))
+# number_two = int(input("Enter Second Number: "))
+# Total_Number = number_one + number_two
+# print("Sum is: " + str(Total_Number))
+#
+number1 = float(str(8))
+number2 = int(3)
+number3 = float(2)
+print(number1 + number3)
