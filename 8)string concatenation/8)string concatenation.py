6b2afc240ff375ded2436c0c1a3886776441cb3a
Ali Raza <coderaliraza860@gmail.com>
2024-11-21 11:38:07 +0500

    Add files via upload

---
+++ 8)string concatenation/8)string concatenation.py
@@ -0,0 +1,7 @@
+first_name = "Ali"
+last_name = "Raza\n"
+full_name = first_name + " " + last_name + " "
+print(full_name)
+# print(full_name + "3")
+# print(full_name + str(6))
+print(full_name * 10)
