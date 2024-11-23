4d7e7520f2d273e98172d3c0c5a49985bc415848
Ali Raza <coderaliraza860@gmail.com>
2024-11-23 21:22:41 +0500

    Update Excersice 3.py

--- 18) Exercise 3/Excersice 3.py
+++ 18) Exercise 3/Excersice 3.py
@@ -1,2 +1,10 @@
 # Ask user name and print back user name in reverse order?
 # Bonus:- Try to make your program in Two line(2) using string formatting?
+
+# solution
+name = input("Enter Your Name: )
+reverse = name[-1::-1]
+print(f"Reverse name is: {reverse}")
+
+
+
