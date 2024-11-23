1e845dfc4d948421bd87d7f22b247d35e1423455
Ali Raza <coderaliraza860@gmail.com>
2024-11-23 21:36:29 +0500

    Add files via upload

---
+++ 19) Python String Methode part 1/19)Python String Methode.py
@@ -0,0 +1,22 @@
+#string methode part one
+name = "Ali raza"
+
+#  1.  len() function
+print(len(name))
+# len function can store another variable
+user_name = len(name)
+print(user_name)
+
+
+#   2. lower() Method
+print(name.lower())
+
+#   3. upper() Method
+print(name.upper())
+
+#   4. title() Method
+print(name.title())
+
+#   5.  count() Method
+print(name.count("A"))
+
