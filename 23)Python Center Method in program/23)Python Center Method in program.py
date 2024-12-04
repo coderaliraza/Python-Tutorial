b66a76cda5162f2da414aed25c9cad2f99889020
Ali Raza <coderaliraza860@gmail.com>
2024-12-04 11:16:41 +0500

    Add files via upload

---
+++ 23)Python Center Method in program/23)Python Center Method in program.py
@@ -0,0 +1,11 @@
+#center method
+name = "Ali"
+#  **Ali**
+print(name.center(4,"*"))
+print(name.center(7,"*"))
+
+
+
+#wite some program
+name = input("Enter Your Name: ")
+print(name.center(len(name)+ 8,"*"))
