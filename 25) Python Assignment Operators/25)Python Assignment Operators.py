10b788abc96243cb5efa743c51900d1c43fec33a
Ali Raza <coderaliraza860@gmail.com>
2024-12-04 11:18:23 +0500

    Add files via upload

---
+++ 25) Python Assignment Operators/25)Python Assignment Operators.py
@@ -0,0 +1,27 @@
+#string add
+name = "Ali"
+name = name + " Raza"
+print(name)
+#shotcut way
+name = "Ali"
+name += "Raza"
+print(name)
+
+
+
+#integer
+age = 23
+age = age + 1
+print(age)
+#shotcut way
+age = 23
+age += 2
+print(age)
+age = 23
+age *= 2
+print(age)
+age = 23
+age -= 2
+print(age)
+
+#any operation are used like (+,-,*)
