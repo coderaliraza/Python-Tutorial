aceb1f7e5bbb9021b53ac41081bb7abb024aeb51
Ali Raza <coderaliraza860@gmail.com>
2024-12-04 11:13:46 +0500

    Add files via upload

---
+++ 21)python solve problem with spaces using strip methode/21)python solve problem with spaces using strip methode.py
@@ -0,0 +1,8 @@
+name = "      A    li Ra     za    "
+dots = "......................"
+print(name+ dots)
+print(name.lstrip() + dots)
+print(name.rstrip() + dots)
+print(name.strip() + dots)
+print(name.replace(" ", "")+ dots)
+
